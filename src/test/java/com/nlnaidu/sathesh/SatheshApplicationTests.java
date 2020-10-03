9b0b97b12e9bb840410ddf048a73276c596c2cf2
Kumar <inskumar38@ad.harman.com>
2020-10-03 19:21:26 +0530

    Sathesh First GITHUB

---
+++ src/test/java/com/nlnaidu/sathesh/SatheshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nlnaidu.sathesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatheshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
